ffe6e9b7058221ce402febbf3dccf83a31e0182f
Byeong Gyu Choi <bisschoi9541@gmail.com>
2017-06-08 21:10:06 +0900

    Binary Search Tree Implementation

--- chapter12/src/datastructure/BinarySearchTree.java
+++ chapter12/src/datastructure/BinarySearchTree.java
@@ -8,13 +8,13 @@ public class BinarySearchTree {
     }
 
     private Node TreeSearch(int k, Node v) {
-        if(v == null) {
+        if (v == null) {
             return v;
         }
 
-        if(k > v.getData()) {
+        if (k > v.getData()) {
             return TreeSearch(k, v.getRight());
-        } else if(k < v.getData()) {
+        } else if (k < v.getData()) {
             return TreeSearch(k, v.getLeft());
         }
 
@@ -23,7 +23,7 @@ public class BinarySearchTree {
 
     public boolean find(int n) {
         Node node = TreeSearch(n, this.root);
-        if(node == null) {
+        if (node == null) {
             return false;
         } else {
             return true;
@@ -31,26 +31,117 @@ public class BinarySearchTree {
     }
 
     public void insert(int value) {
-        if(this.root == null) {
+        if (this.root == null) {
             root = new Node(value);
         }
         insertRecursive(value, root);
     }
 
     public Node insertRecursive(int value, Node node) {
-        if(node == null) {
+        if (node == null) {
             Node newNode = new Node(value);
             return newNode;
-        } else if(value < node.getData()) {
+        } else if (value < node.getData()) {
             Node ret = insertRecursive(value, node.getLeft());
             node.setLeft(ret);
-            return ret;
-        } else if(value > node.getData()) {
+            return node;
+        } else if (value > node.getData()) {
             Node ret = insertRecursive(value, node.getRight());
             node.setRight(ret);
-            return ret;
+            return node;
         }
 
         return node;
+    }
+
+    public boolean delete(int value) {
+        Node parent = root;
+        Node current = root;
+        boolean isLeftChild = false;
+
+        while (current.getData() != value) {
+            parent = current;
+            if (current.getData() > value) {
+                isLeftChild = true;
+                current = current.getLeft();
+            } else {
+                isLeftChild = false;
+                current = current.getRight();
+            }
+            if (current == null) {
+                return false;
+            }
+        }
+
+        // 자식이 1명도 없다
+        if (current.getLeft() == null && current.getRight() == null) {
+            if(isLeftChild) {
+                parent.setLeft(null);
+            } else {
+                parent.setRight(null);
+            }
+        }
+
+        // 자식이 1명만 있다
+        else if (current.getLeft() == null) {
+            if(current == root) {
+                root = current.getRight();
+            } else if(isLeftChild) {
+                parent.setLeft(current.getRight());
+            } else {
+                parent.setRight(current.getRight());
+            }
+        } else if (current.getRight() == null) {
+            if(current == root) {
+                root = current.getLeft();
+            } else if(isLeftChild) {
+                parent.setLeft(current.getLeft());
+            } else {
+                parent.setRight(current.getLeft());
+            }
+        }
+
+        // 자식이 2명 다 있다
+        else if (current.getLeft() != null && current.getRight() != null) {
+            Node successor = getSuccessor(current);
+            if(current == root) {
+                root = successor;
+            } else if(isLeftChild) {
+                parent.setLeft(successor);
+            } else {
+                parent.setRight(successor);
+            }
+            successor.left = current.left;
+        }
+
+        return true;
+    }
+
+    // 오른쪽 서브트리에서 가장 작은 노드를 리턴
+    public Node getSuccessor(Node deleteNode) {
+        Node successor = null;
+        Node successorParent = null;
+        Node current = deleteNode.getRight();
+
+        while(current != null) {
+            successorParent = successor;
+            successor = current;
+            current = current.getLeft();
+        }
+
+        if(successor != deleteNode.right) {
+            successorParent.left = successor.right;
+            successor.right = deleteNode.right;
+        }
+
+        return successor;
+    }
+
+    public void inOrderDisplay(Node node) {
+        if(node != null) {
+            inOrderDisplay(node.left);
+            System.out.print(node.getData() + " ");
+            inOrderDisplay(node.right);
+        }
     }
 }
--- chapter12/src/datastructure/Main.java
+++ chapter12/src/datastructure/Main.java
@@ -3,5 +3,33 @@ package datastructure;
 public class Main {
 
     public static void main(String[] args) {
+        BinarySearchTree bst = new BinarySearchTree();
+        bst.insert(3);
+        bst.insert(8);
+        bst.insert(1);
+        bst.insert(4);
+        bst.insert(6);
+        bst.insert(2);
+        bst.insert(10);
+        bst.insert(9);
+        bst.insert(20);
+        bst.insert(25);
+        bst.insert(15);
+        bst.insert(16);
+
+        System.out.println("original tree : ");
+        bst.inOrderDisplay(bst.root);
+        System.out.println();
+
+        System.out.println("check whether node with value 4 exists : " + bst.find(4));
+        System.out.println("Delete node with no children(2) : " + bst.delete(2));
+        bst.inOrderDisplay(bst.root);
+        System.out.println();
+        System.out.println("Delete node with one child(4) : " + bst.delete(4));
+        bst.inOrderDisplay(bst.root);
+        System.out.println();
+        System.out.println("Delete node with two children(10) : " + bst.delete(10));
+        bst.inOrderDisplay(bst.root);
+        System.out.println();
     }
 }
